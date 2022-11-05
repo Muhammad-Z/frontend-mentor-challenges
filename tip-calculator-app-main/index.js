edbcdb5e911b36c725984a1e45165ad7e84046b5
muhammad-z <mhd.z95@hotmail.com>
2022-11-05 22:29:49 +0200

    Initial Solution. just mobile version

---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,76 @@
+let selectedInput;
+let options = document.getElementById('tip-options');
+
+
+/* document.getElementById('tip-options').onclick = function(event) { */
+function selectTipOption(event) {
+  let target = event.target; // where was the click?
+
+  if (target.tagName !== 'INPUT') return; // not on TD? Then we're not interested
+
+  highlight(target); // highlight it
+};
+
+function highlight(target) {
+  if (selectedInput) { // remove the existing highlight if any
+    selectedInput.classList.remove('highlight');
+  }
+  selectedInput = target;
+  selectedInput.classList.add('highlight'); // highlight the new td
+}
+
+function computeAndShowMoney() {
+  console.log('sup')
+  let bill = document.getElementById('bill-amount').value;
+  let tipValue
+  if (selectedInput && selectedInput.id === 'customTip') tipValue = selectedInput.value;
+  else
+    tipValue = selectedInput ? selectedInput.value.substr(0, selectedInput.value.length - 1) : 0;
+  let tip = tipValue * 0.01
+  let pplCount = document.getElementById('pplCount').value;
+  let tipAmount = bill * tip / pplCount;
+  let total = tipAmount + bill / pplCount;
+  document.getElementById('tipAmount').innerText = tipAmount ? tipAmount.toFixed(2) : (0).toFixed(2);
+  document.getElementById('totalAmount').innerText = total ? total.toFixed(2) : (0).toFixed(2);
+}
+
+Array.from(document.getElementById('tip-options').children).map(elem => elem.addEventListener('click', selectTipOption))
+Array.from(document.querySelectorAll('INPUT')).map(elem => elem.addEventListener('click', computeAndShowMoney))
+
+
+function validateInput(target) {
+  let digitsRegex = /^\d+$/;
+  if (target.value === '')
+    return 'empty'
+  if (!digitsRegex.test(target.value))
+    return 'not a digit';
+  return true;
+}
+
+Array.from(document.querySelectorAll('INPUT')).map(elem => {
+  let oldval;
+  elem.addEventListener('input', (event) => {
+    if (event.target.id === 'pplCount' && validateInput(event.target) === 'empty') {
+      oldval = "";
+      notifyEmpty(true, event.target);
+    }
+    else if (validateInput(event.target) === 'not a digit') {
+      event.target.value = oldval;
+    }
+    else {
+      notifyEmpty(false, event.target);
+      oldval = event.target.value
+    }
+  })
+})
+
+function notifyEmpty(toggle, target) {
+  if (toggle === true) {
+    target.classList.add('error');
+    errorSpan.classList.remove('hidden');
+  }
+  else {
+    target.classList.remove('error');
+    errorSpan.classList.add('hidden');
+  }
+}
