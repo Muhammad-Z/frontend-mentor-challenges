1db389b088d2ce1ebb5d90c4592f7ebf93632cc4
muhammad-z <mhd.z95@hotmail.com>
2022-11-21 22:05:14 +0200

    Initial solution mobile version only

--- todo-app-main/README-template.md
+++ todo-app-main/README-template.md
@@ -68,6 +68,10 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 
 ### What I learned
 
+// while I was trying to remove nodes I learned that getElements, children, nodes are live collections which cause a problem when you mutate it while looping.
+Better aproach in this case is using not live collection by using querySelectorAll
+
+
 Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
 
 To see how you can add code snippets, see below:
---
+++ todo-app-main/index.js
@@ -0,0 +1,111 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+var countBud = 0;
+var activeBtn;
+
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    if (!div.classList.contains('checked')) updateCountBud('decrement');
+    div.remove();
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+    if (ev.target.classList.contains('checked')) updateCountBud('decrement');
+    else updateCountBud('increment');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("elementInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("elementInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  updateCountBud('increment')
+
+  span.onclick = function () {
+    var div = this.parentElement;
+    if (!div.classList.contains('checked')) updateCountBud('decrement');
+    div.remove();
+  }
+}
+
+
+function handleClearCompletedEvent() {
+  var list = document.getElementById('list');
+  let idk = list.querySelectorAll('LI');
+  for (var elem of idk) {
+    if (elem.classList.contains('checked')) /* elem.style.display = "none"; */
+      elem.remove();
+  }
+}
+
+function handleClickAllEvent() {
+  var list = document.getElementById('list');
+  for (var elem of list.getElementsByTagName('LI')) {
+    elem.style.display = "block";
+  }
+}
+
+function handleClickActiveEvent() {
+  var list = document.getElementById('list');
+  for (var elem of list.getElementsByTagName('LI')) {
+    if (elem.classList.contains('checked'))
+      elem.style.display = "none";
+    else elem.style.display = "block";
+  }
+}
+
+function handleClickCompletedEvent() {
+  var list = document.getElementById('list');
+  for (var elem of list.getElementsByTagName('LI')) {
+    if (elem.classList.contains('checked'))
+      elem.style.display = "block";
+    else elem.style.display = "none";
+  }
+}
+
+function updateCountBud(op) {
+  if (op === 'increment')
+    countBud++;
+  else if (op === 'decrement')
+    countBud--;
+  theSpan.innerText = countBud;
+}
+
+function handleActiveBtnClickEvent(event) {
+  if (activeBtn) activeBtn.classList.remove('activeBtn');
+  if (event.target.tagName === 'BUTTON') {
+    activeBtn = event.target;
+    activeBtn.classList.add('activeBtn');
+  }
+}
