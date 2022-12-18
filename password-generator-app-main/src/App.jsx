e404583da65c6d61ee57f99e2b4506e1d58a4940
muhammad-z <mhd.z95@hotmail.com>
2022-12-18 10:21:42 +0300

    Added state for result password

--- password-generator-app-main/src/App.jsx
+++ password-generator-app-main/src/App.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { TasksProvider } from './TasksContext.jsx';
 import PassView from "./PassView.jsx";
 import OptionsForm from "./OptionsForm.jsx";
 import StrengthView from "./StrengthView.jsx";
 import GenerateBtn from "./GenerateBtn.jsx";
 
-export default () => (
-  <>
+export default () => {
+  const [password, setPassword] = useState(false);
+
+  return (<>
     <h1>Password Generator</h1>
     <TasksProvider>
-      <PassView />
+      <PassView password={password} />
       <OptionsForm />
       <StrengthView />
-      <GenerateBtn />
+      <GenerateBtn setPassword={setPassword} />
     </TasksProvider>
   </>
-);
+  )
+};
